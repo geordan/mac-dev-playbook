7822f9ca4779ea5a4713f840bd296ceb0a27ebba
Geordan Liban <geordan@no-reply.github.com>
2022-09-15 22:33:54 -0700

    create python virtual env

--- run.sh
+++ run.sh
@@ -2,10 +2,12 @@
 set -eux
 
 EXTRA_VARS="$1"
+
+python3 -m venv venv
 source venv/bin/activate
 python3 -m pip install --upgrade pip
 python3 -m pip install ansible 
-export PATH="~/Library/Python/3.8/bin:$PATH"
+export PATH="~/Library/Python/3.9/bin:$PATH"
 ansible-galaxy install -r requirements.yml
 ansible-playbook main.yml --ask-become-pass --extra-vars "$EXTRA_VARS"
 deactivate
